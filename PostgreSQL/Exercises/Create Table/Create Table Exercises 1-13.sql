4d01d19e0464cfd473b73682b9414fc529616f88
JacobTGrandView <89894238+JacobTGrandView@users.noreply.github.com>
2023-12-26 23:51:07 -0600

    Add files via upload

---
+++ PostgreSQL/Exercises/Create Table/Create Table Exercises 1-13.sql
@@ -0,0 +1,12 @@
+-- Exercise 1: create a new table called "cars"
+CREATE TABLE cars (
+  brand VARCHAR(255),
+  model VARCHAR(255)
+);
+
+-- Exercise 2: include a integer field called "year"
+CREATE TABLE cars (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT     
+);
