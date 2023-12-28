ce9e4b7961310a940cec1f4803db69f9715729c9
JacobTGrandView <89894238+JacobTGrandView@users.noreply.github.com>
2023-12-27 23:30:34 -0600

    Add files via upload

---
+++ PostgreSQL/Exercises/Create Table/Exercise 5.sql
@@ -0,0 +1,6 @@
+-- Exercise 5: insert 3 new records into the cars table
+INSERT INTO cars (brand, model, year)
+VALUES 
+	('Volvo', 'p1800', 1968), 
+	('BMW', 'M1', 1978),
+	('Toyota', 'Celica', 1975);
---
+++ PostgreSQL/Exercises/Create Table/Exercise 6.sql
@@ -0,0 +1,3 @@
+-- Exercise 6: add a column named colors to the cars table
+ALTER TABLE cars
+ADD color VARCHAR(255);
