2511ee6b4bc4f84c80f5f6dffbf796a292c82c8d
JacobTGrandView <89894238+JacobTGrandView@users.noreply.github.com>
2023-12-28 00:16:58 -0600

    Add files via upload

---
+++ PostgreSQL/Exercises/Operators/Exercise 1.sql
@@ -0,0 +1,3 @@
+-- Exercise 1: select all records where the value of the field year is less than 1970
+SELECT * FROM cars
+WHERE year < 1970;
---
+++ PostgreSQL/Exercises/Operators/Exercise 2.sql
@@ -0,0 +1,3 @@
+-- Exercise 2: select all records where the value of the field model starts with the capital letter 'M'
+SELECT * FROM cars
+WHERE model LIKE 'M%';
