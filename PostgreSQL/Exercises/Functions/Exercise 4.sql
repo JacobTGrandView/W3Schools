a4d5d98f835b47503c60acf4fcf6d9a507ca10b3
JacobTGrandView <89894238+JacobTGrandView@users.noreply.github.com>
2024-01-02 23:44:17 -0600

    Add files via upload

---
+++ PostgreSQL/Exercises/Functions/Exercise 4.sql
@@ -0,0 +1,3 @@
+-- Exercise 4: return sum of all values of the field price in the products table
+SELECT SUM(price)
+FROM products;
---
+++ PostgreSQL/Exercises/Functions/Exercise 5.sql
@@ -0,0 +1,3 @@
+-- Exercise 5: Return average price from products table
+SELECT AVG(price)
+FROM products;
