b10351cdce50edff1dd9707b2b0e048c1fc038c2
JacobTGrandView <89894238+JacobTGrandView@users.noreply.github.com>
2024-01-08 23:18:02 -0600

    Add files via upload

---
+++ JavaScript/Exercises/Operators/Exercise 4.js
@@ -0,0 +1,4 @@
+// Use the correct assignment operator that will result in x being 15
+x = 10;
+y = 5;
+x += y;
---
+++ JavaScript/Exercises/Operators/Exercise 5.js
@@ -0,0 +1,4 @@
+// Use the correct assignment operator that will result in x being 50
+x = 10;
+y = 5;
+x *= y;
